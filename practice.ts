4c0eec1ee15968823d93ca5690a8e8136014cfaa
negii16 <anjalinegi7410@gmail.com>
2026-02-05 19:27:12 +0530

    Day 2

---
+++ practice.ts
@@ -0,0 +1,10 @@
+let fullName: string = "Anjali Negi";
+let age: number = 21;
+let isPremiumUser: boolean = true;
+
+
+function getUserSummary (name:string, age:number): string{
+return `${name} is ${age} years old.`;
+}
+
+getUserSummary('Anjali', 21);
